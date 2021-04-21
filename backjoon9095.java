e946093e8f73958e97417cec0efc0bc14d3dffb9
jaeyoungjang2 <lion85488@gmail.com>
2021-04-21 23:17:08 +0900

    Create backjoon9095.java

---
+++ backjoon9095.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class backjoon9095 {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+
+    }
+
+}

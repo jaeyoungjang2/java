fb40a86da4eeb96fd08a1665a73a9df44fd0f3b2
jaeyoungjang2 <lion85488@gmail.com>
2021-05-19 14:19:57 +0900

    Update backjoon1000.java

--- backjoon1000.java
+++ backjoon1000.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class backjoon1000 {
     public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        Scanner = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
 
@@ -12,4 +12,16 @@ public class backjoon1000 {
 
     }
 
+}
+
+bool prime(int n){
+    if (n < 2) {
+        return false;
+    }
+    for (int i=2; i*i<=n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }

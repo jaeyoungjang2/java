293bc1b59319f78aa57a88310ac96cee5051803e
jaeyoungjang2 <lion85488@gmail.com>
2021-05-20 21:31:05 +0900

    Update backjoon1463.java

--- backjoon1463.java
+++ backjoon1463.java
@@ -6,25 +6,19 @@ public class backjoon1463 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        dp = new int[x + 2];
-        int res = DP(x);
-        System.out.println(res);
+        dp = new int[x + 1];
+        dp[1] = 0;
+
+        for (int i = 2; i <= x; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+            }
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+            }
+        }
+        System.out.println(dp[x]);
     }
 
-    static int DP(int target) {
-        if (target == 1) {
-            return 0;
-        }
-        if (dp[target] > 0) {
-            return dp[target];
-        }
-        dp[target] = DP(target - 1) + 1;
-        if (target % 3 == 0) {
-            dp[target] = Math.min(DP(target / 3) + 1, dp[target]);
-        }
-        if (target % 2 == 0) {
-            dp[target] = Math.min(DP(target / 2) + 1, dp[target]);
-        }
-        return dp[target];
-    }
 }

bb9ad4e7a8dca5ef26d9e3098a9701e2b6247067
jaeyoungjang2 <lion85488@gmail.com>
2021-06-10 22:13:05 +0900

    Update baekjoon2309.java

--- baekjoon2309.java
+++ baekjoon2309.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class baekjoon2309 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int nine[] = new int[9];
-        int seven[] = new int[7];
+        Stack<Integer> seven = new Stack<>();
         for (int i = 0; i < 9; i++) {
             nine[i] = sc.nextInt();
         }
@@ -14,7 +16,28 @@ public class baekjoon2309 {
 
     }
 
-    static void permutation(int[] nine, int[] seven, int num) {
-        System.out.println("HI");
+    static void permutation(int[] nine, Stack seven, int num) {
+        int total = 0;
+        if (num > 9) {
+            break;
+        }
+        if (num == 9) {
+            while (seven.size() > 0) {
+                total += seven.pop();
+            }
+        }
+        if (total == 100) {
+            Arrays.sort(seven);
+            for (int i = 0; i < seven.length; i++) {
+                System.out.println(seven[i]);
+            }
+        }
+        if (seven.length >= 7) {
+            break;
+        }
+        permutation(nine, seven, num+1);
+        seven[num] = nine[num];
+        
+        permutation(nine, seven, num);
     }
 }

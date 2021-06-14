c6e49fb525265e4782b5c8e762a97ce3fd90795d
jaeyoungjang2 <lion85488@gmail.com>
2021-06-14 22:29:25 +0900

    Create StudentMain.java

---
+++ StudentMain.java
@@ -0,0 +1,8 @@
+public class StudentMain {
+    public static void main(String[] args) {
+        Student st = new Student("young", 1, "분석");
+        System.out.println(st.name);
+        System.out.println(st.grade);
+        System.out.println(st.department);
+    }
+}

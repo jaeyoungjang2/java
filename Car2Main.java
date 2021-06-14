f566ab7d5d81a0df7cd0b84cdd110024c0e89483
jaeyoungjang2 <lion85488@gmail.com>
2021-06-14 22:29:15 +0900

    Create Car2Main.java

---
+++ Car2Main.java
@@ -0,0 +1,9 @@
+public class Car2Main {
+    public static void main(String[] args) {
+        Car2 c1 = new Car2();
+        System.out.println(c1.color);
+        System.out.println(c1.name);
+        System.out.println(c1.company);
+        System.out.println(c1);
+    }
+}

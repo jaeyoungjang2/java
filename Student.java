80053261e2ae1330f1a6b182eaa85ebf408dd273
jaeyoungjang2 <lion85488@gmail.com>
2021-06-14 22:29:22 +0900

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    String name;
+    int grade;
+    String department;
+
+    Student(String n, int g, String d) {
+        name = n;
+        grade = g;
+        department = d;
+
+    }
+}

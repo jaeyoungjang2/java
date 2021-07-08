d511abb1c127b11e825e0a42ee60adec249f0bd8
jaeyoungjang2 <lion85488@gmail.com>
2021-07-09 07:06:17 +0900

    Update baekjoon7576.java

--- baekjoon7576.java
+++ baekjoon7576.java
@@ -16,38 +16,70 @@ public class baekjoon7576 {
 
         int[] batchX = { 0, 0, -1, 1 };
         int[] batchY = { -1, 1, 0, 0 };
+        Boolean isTrue = false;
         // 토마토 숙성 상태 저장
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 int state = sc.nextInt();
                 tomatoState[i][j] = state;
+                if (state == -1) {
+                    dayAry[i][j] = -1;
+                }
                 visit[i][j] = true;
                 if (state == 1) {
+                    dayAry[i][j] = 1;
+                    visit[i][j] = false;
                     q.offer(new int[] { i, j });
+                    isTrue = true;
                 }
             }
         }
+        int res = 0;
+        if (isTrue) {
+            while (!q.isEmpty()) {
+                int location[] = q.poll();
+
+                for (int i = 0; i < 4; i++) {
+                    int newH = location[0] + batchX[i];
+                    int newW = location[1] + batchY[i];
+                    if (newH >= 0 && newW >= 0 && newH < n && newW < m && tomatoState[newH][newW] == -1) {
+                        continue;
+                    } else if (newH >= 0 && newW >= 0 && newH < n && newW < m && visit[newH][newW]) {
+                        visit[newH][newW] = false;
+                        dayAry[newH][newW] = dayAry[location[0]][location[1]] + 1;
+                        q.add(new int[] { newH, newW });
+                    }
+                }
+            }
+
+            // 저장될 때 부터 모두 익어있는 상태 (모두 1 ) 이면 0을 출력
+            // 백 트래킹 : 안익은 토마토가 ( 0 ) 존재하면 -1 출력, 안익은 토마토가 ( 0 ) 존재하지 않으면 최소 날짜를 출력
+            for (int i = 0; i < n; i++) {
+                if (res == -1) {
+                    break;
+                }
+                for (int j = 0; j < m; j++) {
+                    if (dayAry[i][j] == 0) {
+                        res = -1;
+                        break;
+                    } else if (dayAry[i][j] > res) {
+                        res = dayAry[i][j];
+                    }
+                }
+            }
+        }
+        if (res != -1) {
+            res -= 1;
+        }
+        System.out.println(res);
+        System.out.println(Arrays.toString(dayAry[0]));
+        System.out.println(Arrays.toString(dayAry[1]));
 
         // 기준점에서 상, 하, 좌, 우에 존재하는 토마토를 숙성시킴
         // 숙성일은 기준점 날짜 + 1일
         // 새로운 좌표는 0이상, y축은 n x축은 m 미만인 값을 지녀야 함
         // tomatoState[][] 의 값이 -1 이면 큐를 추가하지 않는다.
-        while (!q.isEmpty()) {
-            int location[] = q.poll();
 
-            for (int i = 0; i < 4; i++) {
-                int newH = location[0] + batchX[i];
-                int newW = location[1] + batchY[i];
-                if (newH >= 0 && newW >= 0 && newH < n && newW < m && visit[newH][newW]) {
-                    visit[newH][newW] = false;
-                    dayAry[newH][newW] = dayAry[location[0]][location[1]] + 1;
-                    q.add(new int[] { newH, newW });
-                }
-            }
-        }
-
-        // 저장될 때 부터 모두 익어있는 상태 (모두 1 ) 이면 0을 출력
-        // 백 트래킹 : 안익은 토마토가 ( 0 ) 존재하면 -1 출력, 안익은 토마토가 ( 0 ) 존재하지 않으면 최소 날짜를 출력
     }
 
 }

beb3de4b7b4c9316d7e61f3e5a9a06b9214e5e37
jaeyoungjang2 <lion85488@gmail.com>
2021-08-05 06:40:55 +0900

    Create LambdaEx.java

---
+++ LambdaEx.java
@@ -0,0 +1,12 @@
+public class LambdaEx {
+    public static void main(String[] args) {
+        LambdaInterface li = () -> {
+            String str = "메서드 출력";
+            System.out.println(str);
+        };
+    }
+}
+
+interface LambdaInterface {
+    void print();
+}

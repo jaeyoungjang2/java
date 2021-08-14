a1433dce06828150eac3a0fbaa137eb0fbede4d6
jaeyoungjang2 <lion85488@gmail.com>
2021-08-14 16:48:02 +0900

    Create PayrollDayMain.java

---
+++ PayrollDayMain.java
@@ -0,0 +1,7 @@
+public class PayrollDayMain {
+    public static void main(String[] args) {
+        for (EnumEx3 enumEx3 : EnumEx3.values()) {
+            System.out.println(enumEx3);
+        }
+    }
+}

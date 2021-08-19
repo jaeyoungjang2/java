eae9a173e06e061d2360666a6b283b26f19de026
jaeyoungjang2 <lion85488@gmail.com>
2021-08-19 20:49:42 +0900

    Create StreamByIterator.java

---
+++ StreamByIterator.java
@@ -0,0 +1,8 @@
+import java.util.stream.Stream;
+
+public class StreamByIterator {
+    public static void main(String[] args) {
+        Stream<Integer> stream = Stream.iterate(1, n -> n + 1).limit(10);
+        stream.forEach(s -> System.out.println(s));
+    }
+}

4f1f88c3072f52bdb9c1237dbceb9809bd78afdd
jaeyoungjang2 <lion85488@gmail.com>
2021-09-03 11:15:16 +0900

    Create baekjoon2156_2.java

---
+++ baekjoon2156_2.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class baekjoon2156_2 {
+    static int maxResult = 0;
+    static int[] info;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        info = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            info[i] = sc.nextInt();
+        }
+
+        int[][] drinkInfo = new int[num][3];
+
+        drinkInfo = calculateDrinkAmount(info, drinkInfo);
+        for (int i = 0; i < 3; i++) {
+            if (drinkInfo[num - 1][i] > maxResult) {
+                maxResult = drinkInfo[num - 1][i];
+            }
+        }
+        System.out.println(maxResult);
+    }
+
+    static int[][] calculateDrinkAmount(int[] info, int[][] drinkInfo) {
+        if (drinkInfo.length == 1) {
+            maxResult = info[0];
+            return drinkInfo;
+        }
+
+        drinkInfo[0][1] = info[0];
+        for (int i = 1; i < drinkInfo.length; i++) {
+            drinkInfo[i][0] = Math.max(drinkInfo[i - 1][2], drinkInfo[i - 1][1]);
+            drinkInfo[i][1] = drinkInfo[i - 1][0] + info[i];
+            drinkInfo[i][2] = drinkInfo[i - 1][1] + info[i];
+        }
+        return drinkInfo;
+    }
+
+}

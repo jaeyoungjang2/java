9b170bece0ac5df27468212da162ad0d29707666
jaeyoungjang2 <lion85488@gmail.com>
2021-10-09 21:53:00 +0900

    Create Test3.java

---
+++ Test3.java
@@ -0,0 +1,14 @@
+public class Test3 {
+    public static void main(String[] args) {
+        Score score = new Score(10);
+        System.out.println(score.getScore());
+        newTest(score);
+        System.out.println("------- score 수정 -------");
+        System.out.println(score.getScore());
+    }
+
+    public static Score newTest(Score score) {
+        score = new Score(5);
+        return score;
+    }
+}

454948fcfcb872edc59a66983b6c0a311fb51415
jaeyoungjang2 <lion85488@gmail.com>
2021-10-09 21:53:02 +0900

    Create Test4.java

---
+++ Test4.java
@@ -0,0 +1,32 @@
+public class Test4 {
+    public static void main(String[] args) {
+        Score score = new Score(10);
+        System.out.println(score.getScore());
+        System.out.println(score.hashCode());
+        newTest(score);
+        System.out.println("------- score 수정 -------");
+        System.out.println(score.getScore());
+    }
+
+    public static Score newTest(Score score) {
+        System.out.println(score.hashCode());
+        score.setScore(5);
+        return score;
+    }
+}
+
+class Score {
+    private int score;
+
+    public Score(int score) {
+        this.score = score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}

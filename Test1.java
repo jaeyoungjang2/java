6957eee614279829f261491d895926c95a0a2569
jaeyoungjang2 <lion85488@gmail.com>
2021-10-09 19:43:58 +0900

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,22 @@
+class Test1 {
+    private static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    public static void main(String args[]) {
+        int a = 1;
+        int b = 2;
+
+        System.out.println("a => " + a);
+        System.out.println("b => " + b);
+
+        swap(a, b);
+
+        System.out.println("------- swap 후 -------");
+
+        System.out.println("a => " + a);
+        System.out.println("b => " + b);
+    }
+}

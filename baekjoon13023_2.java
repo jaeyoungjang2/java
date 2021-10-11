678c176a6d7e6fda85832d57c896f578606a084f
jaeyoungjang2 <lion85488@gmail.com>
2021-10-11 18:02:05 +0900

    Create baekjoon13023_2.java

---
+++ baekjoon13023_2.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class baekjoon13023_2 {
+    public static void main(String[] args) {
+        // 5 4
+        // 0 1
+        // 1 2
+        // 2 3
+        // 3 4
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}

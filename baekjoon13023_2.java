39f81b77bd4aa49450ee9c355352db8d66d7b95d
jaeyoungjang2 <lion85488@gmail.com>
2021-10-11 19:22:54 +0900

    Update baekjoon13023_2.java

--- baekjoon13023_2.java
+++ baekjoon13023_2.java
@@ -1,14 +1,17 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class baekjoon13023_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 5 4
         // 0 1
         // 1 2
         // 2 3
         // 3 4
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
     }
 }

86e703fbcb5f5c3c4427badc7cb2727c73493487
jaeyoungjang2 <lion85488@gmail.com>
2021-10-12 19:07:08 +0900

    Update baekjoon13023_2.java

--- baekjoon13023_2.java
+++ baekjoon13023_2.java
@@ -12,6 +12,8 @@ public class baekjoon13023_2 {
         // 3 4
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = 
 
     }
 }

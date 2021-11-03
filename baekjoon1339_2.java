3ec1e18884dbb8f3a9d254de6b99d0b7819b8fca
jaeyoungjang2 <lion85488@gmail.com>
2021-11-03 15:13:31 +0900

    Update baekjoon1339_2.java

--- baekjoon1339_2.java
+++ baekjoon1339_2.java
@@ -21,8 +21,8 @@ public class baekjoon1339_2 {
             Integer length = temp.length();
             problemInfo.add(temp);
             for (int j = 0; j < temp.length(); j++) {
-
                 Character alphabet = temp.charAt(j);
+                // 알파벳 마다 자리수 값을 입력
                 if (alphabetScoreMap.containsKey(alphabet)) {
                     alphabetScoreMap.put(alphabet, (int) (alphabetScoreMap.get(alphabet) + Math.pow(10, length - 1)));
                 } else {
@@ -32,9 +32,11 @@ public class baekjoon1339_2 {
             }
         }
 
+        // value 값을 기준으로 정렬
         List<Entry<Character, Integer>> alphabetScoreList = new LinkedList<>(alphabetScoreMap.entrySet());
         Collections.sort(alphabetScoreList, (o1, o2) -> o2.getValue() - o1.getValue());
 
+        // 자리수가 높은 알파벳 부터 9 -> 0 까지 값을 입력
         Map<Character, Integer> map = new HashMap<>();
         int count = 9;
         for (Entry<Character, Integer> entry : alphabetScoreList) {
@@ -42,6 +44,7 @@ public class baekjoon1339_2 {
             count--;
         }
 
+        // 문제에 결과 값을 적용
         int result = 0;
         for (String problem : problemInfo) {
             String temp = "";
